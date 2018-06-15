0942fcd04f8437154c6675783414826c73bee607
oumao <evansotis2015@gmail.com>
2018-06-15 02:35:41 -0600

    new changes for the new repository

--- index.php
+++ index.php
@@ -1,20 +1,35 @@
-<?php
-
-?>
+<?php include 'database.php'; ?>
 <!DOCTYPE html>
 <html>
 <head>
 	<title>Welcome to Online Transcript System</title>
 	<style type="text/css">
 		body{
-			background: lavender;
+			background: #CEB7B3;
 		}
-		h1{
+		h3{
+			text-align: center;
+		}
+		div > h4{
 			text-align: center;
 		}
 	</style>
+	<meta name="viewport" content="width=device-wdth, initial-scale=1.0">
+	 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0-beta/css/materialize.min.css">
 </head>
 <body>
-<h1>Welcome to Online Transcript System</h1>
+<div class="container">
+<header>
+<h3>Welcome to Online Transcript System</h3>
+</header>
+<div>
+	<form class="col s12">
+		<h4>Login</h4>
+	
+	</form>
+</div>
+</div>
+
+
 </body>
 </html>
