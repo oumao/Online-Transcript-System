2d18b21d6bc27df89fc15a31ca7df9b58ac224e4
oumao <evansotis2015@gmail.com>
2018-06-15 01:42:56 -0600

    index.php

---
+++ index.php
@@ -0,0 +1,20 @@
+<?php
+
+?>
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Welcome to Online Transcript System</title>
+	<style type="text/css">
+		body{
+			background: lavender;
+		}
+		h1{
+			text-align: center;
+		}
+	</style>
+</head>
+<body>
+<h1>Welcome to Online Transcript System</h1>
+</body>
+</html>
