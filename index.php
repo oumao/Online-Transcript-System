dfdbb544187ae85a49c078b3964a4ea403202978
oumao <evansotis2015@gmail.com>
2018-06-17 11:38:44 -0600

    Front end is complete

--- index.php
+++ index.php
@@ -2,32 +2,41 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-	<title>Welcome to Online Transcript System</title>
-	<style type="text/css">
-		body{
-			background: skyblue;
-		}
-		h2{
-			text-align: center;
-			color: white;
-		}
-		div > h4{
-			text-align: center;
-		}
-		.card-action{
-			text-align: center;
-		}
-	</style>
+	<title>Transcript System</title>
+	<meta charset="utf-8">
 	<meta name="viewport" content="width=device-wdth, initial-scale=1.0">
-	  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css" integrity="sha384-WskhaSGFgHYWDcbwN70/dfYBj47jz9qbsMId/iRN3ewGhXQFZCSftd1LZCfmhktB" crossorigin="anonymous">
-	
+	<link rel="stylesheet" href="/Transcript_System/bootstrap/dist/css/bootstrap.min.css" style="text/css">
+	<link rel="stylesheet" type="text/css" href="styles.css">
 </head>
 <body>
-<div class="container">
-<header>
-<h2>Welcome to Online Transcript System</h2>
-</header>
-
-</div>
+	<div class="container">
+			<header>
+				<h2>Welcome to Transcript System</h2>
+			</header>
+		<div class="section1">
+			<form role="form" class="form-horizontal">
+				<div class="form-group">
+						<h3>Admin Login</h3>
+						<div class="form-group">
+							<label for="username">Username:</label>
+							<input type="text" name="username" class="form-control" required><br>
+						</div>
+						<div class="form-group">
+							<label for="password">Password:</label>
+							<input type="password" name="password" class="form-control" required><br>
+						</div>
+						<div class="form-group">
+							<input type="submit" name="login" value="Sign In" class="glyphicon glyphicon-user btn btn-primary">
+						</div>
+				</div>
+			</form>
+			<div class="stud">
+				<h3>For Students</h3>
+				<a href="#">Click Here to view Results</a>
+			</div>
+		</div>
+		<footer>Ouma &copy; 2018</footer>
+	</div>
+	<script type="text/javascript" src=""></script>
 </body>
 </html>
